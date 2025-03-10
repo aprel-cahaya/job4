138e7e6d37beb4c669438cf1a02ce335820adf02
aprel-cahaya <apreliacahaya@gmail.com>
2025-03-10 12:11:02 +0700

    Initial Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>profile saya</h1>
+                 </div>
+                 <div class="card-body">
+                    <img src="pp.jpg" alt="profile picture" width="150">
+                    <h3> Aprilia Cahaya kintani</h3>
+                    <p>email: Apreliacahaya@gmail.com</p>
+                    <p>Bio: Backburner</p>
+                   <h5>Skills:</h5>
+                   <ul>
+                    <li>Mukbang </li>
+                    <li>Masak</li>
+                    <li>Healing</li>
+                   </ul>
+                 </div>
+            </div>
+        </div>
+    </body>
+</html>
